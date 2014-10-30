b46d0cb5bdf6d81ac0d3fc1041c38c2c419b10dc
hyperone <logan.grauer@uponor.com>
2014-10-30 15:37:44 -0500

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,12 +12,12 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
+	var defaultKey		= 'NenoEZfJrkHc0hob0uu17gX4OgucdxDdN2WraX2ERZ3xAjko', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [1855538582], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Domestic Hot Water Tank heat Enable', // Replaces Xively logo in the header
+		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
+		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
